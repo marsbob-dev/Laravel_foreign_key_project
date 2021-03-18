138198fd1d9612c43bd04bf432c651b987c636a0
marsbob-dev <73162318+marsbob-dev@users.noreply.github.com>
2021-03-18 12:52:51 +0100

    seeder avatar ok

---
+++ database/seeders/AvatarSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AvatarSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('avatars')->insert([
+            [
+                "name" => "default",
+                "src" => "default-avatar.jpg"
+            ]
+        ]);
+    }
+}
